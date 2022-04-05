3fdb66813a386b588a0043594d5fc173feedf7b9
Raizel <ngominhhthai2001@gmail.com>
2022-04-05 12:14:49 +0700

    Update how to standard data

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ from utils import CvFpsCalc
 from model import KeyPointClassifier
 from model import PointHistoryClassifier
 
+HAND_LANDMARKS_LEN = 21
+HISTORY_LEN = 16
+DIMENSION = 2
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -90,11 +93,11 @@ def main():
     cvFpsCalc = CvFpsCalc(buffer_len=10)
 
     # Coordinate history #################################################################
-    history_length = 24
-    point_history = deque(maxlen=history_length)
+    general_hand_landmark_history = deque(maxlen=HISTORY_LEN)
+    specific_hand_landmark_history = deque(maxlen=HISTORY_LEN)
 
     # Finger gesture history ################################################
-    finger_gesture_history = deque(maxlen=history_length)
+    # finger_gesture_history = deque(maxlen=HISTORY_LEN)
 
     #  ########################################################################
     mode = 0
@@ -129,36 +132,57 @@ def main():
                 # Bounding box calculation
                 brect = calc_bounding_rect(debug_image, hand_landmarks)
                 # Landmark calculation
-                landmark_list = calc_landmark_list(debug_image, hand_landmarks)
+                specific_landmark_list = calc_specific_landmark_list(debug_image, hand_landmarks)
+                general_landmark_list = calc_general_landmark_list(hand_landmarks)
+
+                # base_x, base_y = general_landmark_list[0][0], general_landmark_list[0][1]
+                # for point in general_landmark_list:
+                #     point[0] = point[0] - base_x
+                #     point[1] = point[1] - base_y 
+
+                # for point in general_landmark_list:
+                #     point[0] = point[0] * debug_image.shape[1] * 0.0000002 + debug_image.shape[1] / 2
+                #     point[1] = point[1] * debug_image.shape[0] * 0.0000002 + debug_image.shape[0] / 2
+
+                # for point in general_landmark_list:
+                #     cv.circle(debug_image, (int(point[0]), int(point[1])), 5,
+                #             (152, 251, 152), 2)
+                # print(general_landmark_list)
 
                 # Conversion to relative coordinates / normalized coordinates
-                pre_processed_landmark_list = pre_process_landmark(
-                    landmark_list)
-                pre_processed_point_history_list = pre_process_point_history(
-                    debug_image, point_history)
+                pre_processed_general_landmark_list = pre_process_general_landmark(
+                    general_landmark_list)
+                # print(pre_processed_general_landmark_list)
+
+                pre_processed_general_hand_landmark_history_list = pre_process_general_hand_landmark_history(
+                    general_hand_landmark_history
+                )
+                # print(pre_processed_general_hand_landmark_history_list)
+
                 # Write to the dataset file
-                logging_csv(number, mode, pre_processed_landmark_list,
-                            pre_processed_point_history_list)
+                logging_csv(number, mode, pre_processed_general_landmark_list,
+                            pre_processed_general_hand_landmark_history_list)
 
                 # Hand sign classification
-                hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
-
-                point_history.append(landmark_list)
+                # hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
+        
+                specific_hand_landmark_history.append(specific_landmark_list)
+                general_hand_landmark_history.append(general_landmark_list)
 
                 # Finger gesture classification
-                finger_gesture_id = 0
-                point_history_len = len(pre_processed_point_history_list)
-                if point_history_len == (history_length * 2 * 21):
-                    finger_gesture_id = point_history_classifier(pre_processed_point_history_list)
-                    if finger_gesture_id == -1:
-                        print("Not recog")
-                    else:
-                        print(point_history_classifier_labels[finger_gesture_id])
+                # finger_gesture_id = 0
+                # point_history_len = len(pre_processed_point_history_list)
+                # if point_history_len == (HISTORY_LEN * 2 * 21):
+                #     finger_gesture_id = point_history_classifier(pre_processed_point_history_list)
+                    # if finger_gesture_id == -1:
+                    #     print("Not recog")
+                    # else:
+                    #     print(point_history_classifier_labels[finger_gesture_id])
 
                 # Calculates the gesture IDs in the latest detection
-                finger_gesture_history.append(finger_gesture_id)
-                most_common_fg_id = Counter(
-                    finger_gesture_history).most_common()
+                # finger_gesture_history.append(finger_gesture_id)
+                # most_common_fg_id = Counter(
+                #     finger_gesture_history).most_common()
 
                 # Drawing part
                 debug_image = draw_bounding_rect(debug_image, brect)
@@ -167,17 +191,19 @@ def main():
                     (results.multi_hand_landmarks)[0],
                     mp_hands.HAND_CONNECTIONS
                 )
-                debug_image = draw_info_text(
-                    debug_image,
-                    brect,
-                    handedness,
-                    keypoint_classifier_labels[hand_sign_id],
-                    point_history_classifier_labels[most_common_fg_id[0][0]],
-                )
+                        
+                # debug_image = draw_info_text(
+                #     debug_image,
+                #     brect,
+                #     handedness,
+                #     keypoint_classifier_labels[hand_sign_id],
+                #     point_history_classifier_labels[most_common_fg_id[0][0]],
+                # )
         else:
-            point_history.append([[0, 0]] * 21)
+            specific_hand_landmark_history.append([[0, 0]] * 21)
+            general_hand_landmark_history.append([[0, 0]] * 21)
 
-        debug_image = draw_point_history(debug_image, point_history)
+        debug_image = draw_point_history(debug_image, specific_hand_landmark_history)
         debug_image = draw_info(debug_image, fps, mode, number)
 
         # Screen reflection #############################################################
@@ -202,97 +228,87 @@ def select_mode(key, mode):
 
 def calc_bounding_rect(image, landmarks):
     image_width, image_height = image.shape[1], image.shape[0]
-
     landmark_array = np.empty((0, 2), int)
 
     for _, landmark in enumerate(landmarks.landmark):
         landmark_x = min(int(landmark.x * image_width), image_width - 1)
         landmark_y = min(int(landmark.y * image_height), image_height - 1)
-
         landmark_point = [np.array((landmark_x, landmark_y))]
-
         landmark_array = np.append(landmark_array, landmark_point, axis=0)
 
     x, y, w, h = cv.boundingRect(landmark_array)
-
     return [x, y, x + w, y + h]
 
 
-def calc_landmark_list(image, landmarks):
-    image_width, image_height = image.shape[1], image.shape[0]
-
-    landmark_point = []
-
+def calc_specific_landmark_list(image, landmarks):
+    image_width, image_height =  image.shape[1], image.shape[0]
+    landmark_points = []
+    
     # Keypoint
     for _, landmark in enumerate(landmarks.landmark):
-        landmark_x = min(int(landmark.x * image_width), image_width - 1)
-        landmark_y = min(int(landmark.y * image_height), image_height - 1)
+        landmark_x = int(landmark.x * image_width)
+        landmark_y = int(landmark.y * image_height)
         # landmark_z = landmark.z
+        landmark_points.append([landmark_x, landmark_y])
 
-        landmark_point.append([landmark_x, landmark_y])
+    # for index, landmark_point in enumerate(landmark_points):
+    #     if index == 0:
+    #         base_x, base_y = landmark_point[0], landmark_point[1]
 
-    return landmark_point
+    #     landmark_points[index][0] = landmark_points[index][0] - base_x
+    #     landmark_points[index][1] = landmark_points[index][1] - base_y
+
+    return landmark_points
 
 
-def pre_process_landmark(landmark_list):
-    temp_landmark_list = copy.deepcopy(landmark_list)
+def calc_general_landmark_list(landmarks):
+    landmark_points = []
+    
+    # Keypoint
+    normalize_factor = 1
+    for index, landmark in enumerate(landmarks.landmark):
+        if index == 0:
+            normalize_factor = abs(landmark.z)
 
-    # Convert to relative coordinates
-    base_x, base_y = 0, 0
-    for index, landmark_point in enumerate(temp_landmark_list):
+        landmark_x = landmark.x / normalize_factor 
+        landmark_y = landmark.y / normalize_factor
+        # landmark_z = landmark.z
+        landmark_points.append([landmark_x, landmark_y])
+
+    return landmark_points
+
+
+def pre_process_general_landmark(general_landmark_list):
+    temp_general_landmark_list = copy.deepcopy(general_landmark_list)
+    for index, landmark_point in enumerate(temp_general_landmark_list):
         if index == 0:
             base_x, base_y = landmark_point[0], landmark_point[1]
 
-        temp_landmark_list[index][0] = temp_landmark_list[index][0] - base_x
-        temp_landmark_list[index][1] = temp_landmark_list[index][1] - base_y
+        temp_general_landmark_list[index][0] = temp_general_landmark_list[index][0] - base_x
+        temp_general_landmark_list[index][1] = temp_general_landmark_list[index][1] - base_y
 
-    # Convert to a one-dimensional list
-    temp_landmark_list = list(
-        itertools.chain.from_iterable(temp_landmark_list))
-
-    # Normalization
-    max_value = max(list(map(abs, temp_landmark_list)))
-
-    def normalize_(n):
-        return n / max_value
-
-    temp_landmark_list = list(map(normalize_, temp_landmark_list))
-
-    return temp_landmark_list
+    return [ele for row in temp_general_landmark_list for ele in row]
 
 
-def pre_process_point_history(image, hand_history):
-    image_width, image_height = image.shape[1], image.shape[0]
-
-    temp_hand_history = copy.deepcopy(hand_history)
+def pre_process_general_hand_landmark_history(hand_landmark_history):
+    temp_hand_landmark_history = copy.deepcopy(hand_landmark_history)
 
     # Convert to relative coordinates
-    base_x, base_y = [0] * 21, [0] * 21
-    for index, hand in enumerate(temp_hand_history):
+    base_x, base_y = 0, 0
+    for index, hand in enumerate(temp_hand_landmark_history):
         for point_id in range(len(hand)):
             if index == 0:
-                base_x[point_id], base_y[point_id] = hand[point_id][0], hand[point_id][1]
+                base_x, base_y = hand[0][0], hand[0][1]
 
-            temp_hand_history[index][point_id][0] = (temp_hand_history[index][point_id][0] - base_x[point_id]) / image_width
-            temp_hand_history[index][point_id][1] = (temp_hand_history[index][point_id][1] - base_y[point_id]) / image_height
-        # for point in hand:
-        #     if index == 0:
-        #         base_x, base_y = point[0], point[1]
-
-        #     temp_point_history[index][0] = (temp_point_history[index][0] -
-        #                                     base_x) / image_width
-        #     temp_point_history[index][1] = (temp_point_history[index][1] -
-        #                                     base_y) / image_height
+            temp_hand_landmark_history[index][point_id][0] = temp_hand_landmark_history[index][point_id][0] - base_x
+            temp_hand_landmark_history[index][point_id][1] = temp_hand_landmark_history[index][point_id][1] - base_y
 
     # Convert to a one-dimensional list 
     res = []
-    for index, hand in enumerate(temp_hand_history):
+    for index, hand in enumerate(temp_hand_landmark_history):
         for point_id in range(len(hand)):
-            res += [temp_hand_history[index][point_id][0]]
-            res += [temp_hand_history[index][point_id][1]]
-    # temp_hand_history = list(
-    #     itertools.chain.from_iterable(temp_hand_history))
-    # print(temp_hand_history)
+            res += [temp_hand_landmark_history[index][point_id][0]]
+            res += [temp_hand_landmark_history[index][point_id][1]]
     return res
 
 
@@ -310,7 +326,6 @@ def logging_csv(number, mode, landmark_list, point_history_list):
             writer = csv.writer(f)
             writer.writerow([number, *point_history_list])
     return
-
 
 
 def draw_bounding_rect(image, brect):
@@ -340,6 +355,20 @@ def draw_info_text(image, brect, handedness, hand_sign_text,
 
 
 def draw_point_history(image, hand_history):
+    # temp_hand_landmark_history = copy.deepcopy(hand_history)
+    # Convert to relative coordinates
+
+    # for index, hand in enumerate(temp_hand_landmark_history):
+    #     for point_id in range(len(hand)):
+    #         temp_hand_landmark_history[index][point_id][0] = temp_hand_landmark_history[index][point_id][0] * image.shape[1] * 0.0000002
+    #         temp_hand_landmark_history[index][point_id][1] = temp_hand_landmark_history[index][point_id][1] * image.shape[0] * 0.0000002
+
+    # base_x, base_y = temp_hand_landmark_history[0][0][0], temp_hand_landmark_history[0][0][1]
+    # for index, hand in enumerate(temp_hand_landmark_history):
+    #     for point_id in range(len(hand)):
+    #         temp_hand_landmark_history[index][point_id][0] = temp_hand_landmark_history[index][point_id][0] - base_x + image.shape[1] / 2
+    #         temp_hand_landmark_history[index][point_id][1] = temp_hand_landmark_history[index][point_id][1] - base_y + image.shape[0] / 2
+
     for index, hand in enumerate(hand_history):
         for point in hand:
             if point[0] != 0 and point[1] != 0:
